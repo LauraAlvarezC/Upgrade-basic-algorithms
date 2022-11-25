2054404d7e5e4053eddba2c1d6ee38e197016041
laura AC <lauacorti@gmail.com>
2022-11-25 23:25:02 +0100

    Iteration#3

---
+++ Ejercicio3.js
@@ -0,0 +1,29 @@
+
+let num1 = 10
+let num2 = 5
+let resultado = (num1*num2)
+console.log(resultado)
+
+
+let num3 = 2
+let division = (num1/num3)
+console.log(division)
+
+
+
+let num4 = 15
+let num5 = 9
+let modulo = num4%num5
+console.log(modulo)
+
+
+
+let y = 10
+let z = 5
+
+let x = (y+z)
+console.log(x)
+
+
+ x = (y*z)
+console.log(x)
