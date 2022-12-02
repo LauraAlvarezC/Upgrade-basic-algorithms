3c20bdb9a08398b6e2755618ae3a60dd640c43b1
laura AC <lauacorti@gmail.com>
2022-12-02 19:55:26 +0100

    Iteration#4

---
+++ Ejercicio4.js
@@ -0,0 +1,37 @@
+
+const avengers =["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log(avengers[0]);
+
+
+
+avengers[0] = "IRONMAN";
+console.log(avengers)
+
+
+
+
+console.log(avengers.length);
+
+
+
+
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push("Morty", "Summer");
+
+console.log(rickAndMortyCharacters[4]);
+
+
+
+
+const rickAndMortyCharacters2  = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+rickAndMortyCharacters2.pop()
+
+console.log(rickAndMortyCharacters2[0]);
+console.log(rickAndMortyCharacters2.length-1);
+
+
+rickAndMortyCharacters2.splice(1,1);
+console.log(rickAndMortyCharacters2);
+
+
